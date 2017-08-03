3c70fcafd5eba5f58c841f1fc860eb86e6a17e12
k-furukawa <townewgokgok@gmail.com>
2017-08-04 07:42:23 +0900

    Refactor

--- internal/settings/templates.go
+++ internal/settings/templates.go
@@ -5,7 +5,11 @@ import (
 	"sort"
 	"strconv"
 
+	"strings"
+
 	"github.com/kyokomi/emoji"
+	"github.com/townewgokgok/slack-status/internal/helper"
+	"github.com/townewgokgok/slack-status/internal/music"
 )
 
 type TemplateSettings map[string]string
@@ -27,4 +31,50 @@ func (s TemplateSettings) Dump(indent string) string {
 		result += emoji.Sprint(str)
 	}
 	return result
+}
+
+func Generate(templateIDs []string) (string, string, []string, error) {
+	notice := []string{}
+
+	texts := []string{}
+	for _, id := range templateIDs {
+		tmpl, ok := Settings.Templates[id]
+		if ok {
+			texts = append(texts, tmpl)
+			continue
+		}
+		switch id {
+		case Settings.ITunes.TemplateID:
+			status := &music.GetITunesStatus().MusicStatus
+			if status.Ok {
+				texts = append(texts, Settings.ITunes.MusicSettings.ReplacePlaceholder(status))
+				continue
+			}
+			n := "iTunes seems to be stopped"
+			if status.Err != "" {
+				n += " : " + status.Err
+			}
+			notice = append(notice, n)
+		case Settings.LastFM.TemplateID:
+			status := &music.GetLastFMStatus(Settings.LastFM.APIKey, Settings.LastFM.UserName).MusicStatus
+			if status.Ok {
+				texts = append(texts, Settings.LastFM.MusicSettings.ReplacePlaceholder(status))
+				continue
+			}
+			n := "Failed to fetch music information from last.fm"
+			if status.Err != "" {
+				n += " : " + status.Err
+			}
+			notice = append(notice, n)
+		default:
+			return "", "", nil, fmt.Errorf(
+				`Template "%s" is not defined in settings file.`+"\n"+
+					`Try "slack-status list" to list your templates.`,
+				id,
+			)
+		}
+	}
+
+	emj, txt := helper.SplitEmoji(strings.Join(texts, " "))
+	return emj, txt, notice, nil
 }
--- internal/slack/update.go
+++ internal/slack/update.go
@@ -5,10 +5,7 @@ import (
 	"strings"
 	"time"
 
-	"fmt"
-
 	"github.com/townewgokgok/slack-status/internal/helper"
-	"github.com/townewgokgok/slack-status/internal/music"
 	s "github.com/townewgokgok/slack-status/internal/settings"
 )
 
@@ -18,56 +15,19 @@ var updatedCount int
 
 func Update(watch, dryRun bool, templateIDs []string) error {
 	now := time.Now().Format("[15:04:05] ")
-	notice := []string{}
 
-	tmpls := []string{}
-	for _, id := range templateIDs {
-		tmpl, ok := s.Settings.Templates[id]
-		if ok {
-			tmpls = append(tmpls, tmpl)
-			continue
-		}
-		switch id {
-		case s.Settings.ITunes.TemplateID:
-			status := &music.GetITunesStatus().MusicStatus
-			if status.Ok {
-				tmpls = append(tmpls, s.Settings.ITunes.MusicSettings.ReplacePlaceholder(status))
-				continue
-			}
-			n := "iTunes seems to be stopped"
-			if status.Err != "" {
-				n += " : " + status.Err
-			}
-			notice = append(notice, n)
-		case s.Settings.LastFM.TemplateID:
-			status := &music.GetLastFMStatus(s.Settings.LastFM.APIKey, s.Settings.LastFM.UserName).MusicStatus
-			if status.Ok {
-				tmpls = append(tmpls, s.Settings.LastFM.MusicSettings.ReplacePlaceholder(status))
-				continue
-			}
-			n := "Failed to fetch music information from last.fm"
-			if status.Err != "" {
-				n += " : " + status.Err
-			}
-			notice = append(notice, n)
-		default:
-			return fmt.Errorf(
-				`Template "%s" is not defined in settings file.`+"\n"+
-					`Try "slack-status list" to list your templates.`,
-				id,
-			)
-		}
+	emj, txt, notice, err := s.Generate(templateIDs)
+	if err != nil {
+		return err
 	}
 
-	t := strings.Join(tmpls, " ")
-	e, t := helper.SplitEmoji(t)
-	t = helper.LimitStringByLength(t, SlackUserStatusMaxLength)
-	changed := updatedCount == 0 || !(t == lastText && e == lastEmoji)
+	txt = helper.LimitStringByLength(txt, SlackUserStatusMaxLength)
+	changed := updatedCount == 0 || !(txt == lastText && emj == lastEmoji)
 
 	if changed {
 		var err error
 		if !dryRun {
-			err = SetSlackUserStatus(t, e)
+			err = SetSlackUserStatus(txt, emj)
 		}
 		if 0 < len(notice) {
 			if watch {
@@ -78,14 +38,14 @@ func Update(watch, dryRun bool, templateIDs []string) error {
 		if watch {
 			helper.Cyan.Print(now)
 		}
-		helper.PrintStatus(e, t)
+		helper.PrintStatus(emj, txt)
 		if err != nil {
 			return err
 		}
 	}
 
-	lastText = t
-	lastEmoji = e
+	lastText = txt
+	lastEmoji = emj
 	updatedCount++
 
 	return nil
